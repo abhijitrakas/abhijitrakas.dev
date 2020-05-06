e84d76732f63317c24b6fe18327969e26f3b320e
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-05-06 09:46:25 +0530

    Add sitemap, GA, and robotstxt. Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Just another human blog`,
     description: `WordPress, React, Gutenberg, JavaScript`,
     author: `Abhijit Rakas <abhijitrakas@gmail.com>`,
+    siteUrl: 'https://abhijitrakas.dev',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `abhijitrakas.dev`,
+        short_name: `Abhijit Blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/header.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
@@ -39,6 +40,23 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `@wordpress/scripts`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-JM8WJS3MV2",
+        // this option places the tracking script into the head of the DOM
+        head: true,
+        // other options
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://abhijitrakas.dev',
+        sitemap: 'https://abhijitrakas.dev/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    }
   ],
 }
